2024ec71e9f5b20ded6f7dc0ea9e92a1a9e1b293
luyiyun <18845728185@163.com>
2019-08-27 20:12:27 +0800

    add mse discriminator

--- train.py
+++ train.py
@@ -33,7 +33,8 @@ class BatchEffectTrainer:
     def __init__(
         self, in_features, bottle_num, no_be_num, lrs=0.01, bs=64, nw=6,
         epoch=100, device=torch.device('cuda:0'), l2=0.0, clip_grad=False,
-        ae_disc_train_num=(1, 1), no_best=False, ae_disc_weight=(1.0, 5.0)
+        ae_disc_train_num=(1, 1), no_best=False, ae_disc_weight=(1.0, 5.0),
+        mse=True
     ):
 
         # 得到3个模型
@@ -57,7 +58,7 @@ class BatchEffectTrainer:
 
         # 得到两个loss
         self.criterions = {
-            'adversarial': nn.CrossEntropyLoss(),
+            'adversarial': nn.CrossEntropyLoss() if not mse else nn.MSELoss(),
             'reconstruction': nn.MSELoss()
         }
 
@@ -76,11 +77,15 @@ class BatchEffectTrainer:
                 self.models['discriminator'].parameters(), lr=lrs[1]
             )
         }
+        self.scheduals = optim.lr_scheduler.MultiStepLR(
+            self.optimizers['discriminate'],
+            [500], gamma=0.1
+        )
 
         # 初始化结果记录
         self.history = {
-            'autoencode_loss': [], 'discriminate_loss': [],
-            'reconstruction_loss': []
+            'reconstruction_loss': [], 'discriminate_loss_train': [],
+            'discriminate_loss_fixed': []
         }
 
         # 属性化
@@ -93,6 +98,7 @@ class BatchEffectTrainer:
         self.bs = bs
         self.nw = nw
         self.no_be_num = no_be_num
+        self.mse = mse
 
         # 可视化工具
         self.visobj = VisObj()
@@ -112,26 +118,33 @@ class BatchEffectTrainer:
             ## train phase
             # reset = True  # 用于visdom的方法，绘制batch的loss曲线，指示是否是append的
             # 实例化3个loss对象，用于计算epoch loss
-            ad_loss_obj = mm.Loss()
-            ae_loss_obj = mm.Loss()
+            ad_loss_train_obj = mm.Loss()
+            ad_loss_fixed_obj = mm.Loss()
             recon_loss_obj = mm.Loss()
             # 训练时需要将模型更改至训练状态
             for model in self.models.values():
                 model.train()
+            self.scheduals.step()
             # 循环每个batch进行训练
             for batch_x, batch_y in tqdm(dataloaders['train'], 'Batch: '):
                 batch_x = batch_x.to(self.device, torch.float)
                 batch_y = batch_y.to(self.device, torch.long).squeeze()
+                if self.mse:
+                    batch_y_oh = torch.arange(4).diag()[batch_y].float().to(
+                        self.device)
+                else:
+                    batch_y_oh = batch_y
                 for optimizer in self.optimizers.values():
                     optimizer.zero_grad()
                 for _ in range(self.discriminate_train_num):
-                    ad_loss = self._forward_discriminate(batch_x, batch_y)
+                    ad_loss_train = self._forward_discriminate(
+                        batch_x, batch_y_oh)
                 for _ in range(self.autoencode_train_num):
-                    ae_loss, recon_loss, _ = self._forward_autoencode(
-                        batch_x, batch_y)
+                    recon_loss, ad_loss_fixed = self._forward_autoencode(
+                        batch_x, batch_y_oh)
                 # 记录loss(这样，就算update了多次loss，也只记录最后一次)
-                ad_loss_obj.add(ad_loss, batch_x.size(0))
-                ae_loss_obj.add(ae_loss, batch_x.size(0))
+                ad_loss_train_obj.add(ad_loss_train, batch_x.size(0))
+                ad_loss_fixed_obj.add(ad_loss_fixed, batch_x.size(0))
                 recon_loss_obj.add(recon_loss, batch_x.size(0))
                 # 可视化batch loss
                 # self.visobj.add_batch_loss(
@@ -139,12 +152,14 @@ class BatchEffectTrainer:
                 # reset = False
 
             # 记录并可视化epoch loss
-            self.history['autoencode_loss'].append(ae_loss_obj.value())
-            self.history['discriminate_loss'].append(ad_loss_obj.value())
+            self.history['discriminate_loss_train'].append(
+                ad_loss_train_obj.value())
+            self.history['discriminate_loss_fixed'].append(
+                ad_loss_fixed_obj.value())
             self.history['reconstruction_loss'].append(recon_loss_obj.value())
             self.visobj.add_epoch_loss(
-                ae=self.history['autoencode_loss'][-1],
-                ad=self.history['discriminate_loss'][-1],
+                disc_train=self.history['discriminate_loss_train'][-1],
+                disc_fixed=self.history['discriminate_loss_fixed'][-1],
                 recon=self.history['reconstruction_loss'][-1]
             )
 
@@ -218,7 +233,7 @@ class BatchEffectTrainer:
                 ), max_norm=1
             )
         self.optimizers['autoencode'].step()
-        return all_loss, reconstruction_loss, adversarial_loss
+        return reconstruction_loss, adversarial_loss
 
     def _forward_discriminate(self, batch_x, batch_y):
         with torch.set_grad_enabled(False):
@@ -287,9 +302,6 @@ def main():
     config.show()
 
     # ----- 读取数据 -----
-    # pre_transfer = T.MultiCompose(*[
-    #     T.Normalization()
-    # ])
     meta_data = MetaBatchEffect.from_csv(
         config.sample_file, config.meta_file, 'batch',
         pre_transfer=T.MultiCompose(
@@ -315,10 +327,10 @@ def main():
     print('')
 
     # 保存结果
-    # dirname = check_update_dirname(config.args.save)
-    # torch.save(best_models, os.path.join(dirname, 'models.pth'))
-    # pd.DataFrame(hist).to_csv(os.path.join(dirname, 'train.csv'))
-    # config.save(os.path.join(dirname, 'config.json'))
+    dirname = check_update_dirname(config.args.save)
+    torch.save(best_models, os.path.join(dirname, 'models.pth'))
+    pd.DataFrame(hist).to_csv(os.path.join(dirname, 'train.csv'))
+    config.save(os.path.join(dirname, 'config.json'))
 
 
 if __name__ == "__main__":
